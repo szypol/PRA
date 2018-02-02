392ba747741151d32c1a66131730971e5ed35134
Szymon <szypolak@gmail.com>
2018-02-02 14:50:35 +0100

    *It works!

--- PracowaniaProgramowania/src/main/java/Project3/repositories/ClientRepository.java
+++ PracowaniaProgramowania/src/main/java/Project3/repositories/ClientRepository.java
@@ -8,9 +8,6 @@ import org.springframework.data.repository.PagingAndSortingRepository;
 public interface ClientRepository extends CrudRepository<Client, Integer>, PagingAndSortingRepository<Client, Integer> {
 
     Client findByClientId(Integer clientId);
-    //Client getByNIP(int NIP);
-    //Client getByFirstNameAndLastName(String firstName, String lastName);
-    //Client getByCompanyNameAndLocality(String companyName, String locality);
 
     @Query("select count(*) from Client c where c.clientId = ?1")
     Integer checkIfExist(Integer clientId);
